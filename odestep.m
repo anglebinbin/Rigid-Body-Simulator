336e2054e56e462c7f9df6765ba266c13f5bffc4
孙旻 <minsunchina@gmail.com>
2015-12-10 05:01:44 -0500

    Plane pendulum model
    A plane pendulum model with three integration methods: Euler's Method,
    The Improved Euler Method, and Runge-Kutte Integration Method.

--- odestep.m
+++ odestep.m
@@ -1,22 +1,25 @@
-function x2 = odestep(x1, f, h, solver,fparams)
-  % given current state x1 and time-step h, with dynamics f, compute next
-  % step
-  %
-  % Note: to call f, run "feval(f,x,fparams)"
-  
-  
-  if strcmp(solver, 'euler')
-    x2 = 0;  % TODO: write this
-  else 
+function x2 = odestep(x1, f, h, solver, fparams)
+% given current state x1 and time-step h, with dynamics f, compute next
+% step
+%
+% Note: to call f, run "feval(f,x,fparams)"
+
+
+if strcmp(solver, 'euler')
+    x2 = x1 + h * feval(f, x1, fparams);    
+else
     if strcmp(solver, 'imp_euler')
-      x2 = 0; % TODO: write this
+        x2 = x1 + h / 2 * ( feval(f, x1, fparams) + feval(f, h * feval(f, x1, fparams), fparams));
     else
-      if strcmp(solver, 'rk4')
-	x2 = 0; % TODO: write this
-      else 
-	error('Unknown solver specified')
-      end
+        if strcmp(solver, 'rk4')
+            k1 = h * feval(f, x1, fparams);
+            k2 = h * feval(f, x1 + k1/2, fparams);
+            k3 = h * feval(f, x1 + k2/2, fparams);
+            k4 = h * feval(f, x1 + k3, fparams);
+            x2 = x1 + (k1 + 2*k2 + 2*k3 + k4) / 6;
+        else
+            error('Unknown solver specified')
+        end
     end
-  end
-  
-    
+end
+
--- runODE.m
+++ runODE.m
@@ -1,33 +1,49 @@
 % set up the parameters
-h = .1;
-x0 = [pi/2, 0]';
-nsteps = 200;
-len = 5;
-g = 9.8;
-m = 1;
-rho = 0;
-params = [rho/(m*len*len),g/len];
+h       = 0.1;           % time step
+x0      = [pi/2, 0]';   % initial state
+nsteps  = 200;          % number of steps
+len     = 5;            % length of the rod
+g       = 9.8;          % gravitational constant
+m       = 1;            % mass of the particle
+rho     = 0.0;          % friction factor
+params  = [rho/(m*len*len), g/len]; % squre of omega
 
 X = zeros(2,nsteps);
 X(:,1) = x0;
 
-T = zeros(1,nsteps);
-U = zeros(1,nsteps);
+T = zeros(1,nsteps);        % kinetic energy
+U = zeros(1,nsteps);        % potential energy
+Total = zeros(1, nsteps);	% total energy
 
-for i=1:nsteps-1
-  fprintf('Step: %03d, time: %3.2f     \r',i, h*i);
-  
-  subplot(1,2,1, 'replace');
-  title('pendulum')
-  drawnow
-  
-  subplot(1,2,2);
-  title('energies')
-  % TODO: plot the energy up to now
-  % label the axes and put a legend
-  
-  
-  % update the state & energies
-  X(:,i+1) = odestep(X(:,i),@pend,h,'rk4',params);
-
+for i = 1:nsteps-1
+    fprintf('Step: %03d, time: %3.2f     \r',i, h*i);
+    
+    subplot(1,2,1, 'replace');
+    title('pendulum')
+    axis square
+    drawPendulum(X(1,i));
+    drawnow
+    
+    T(i) = m * (len * X(2, i))^2 / 2;
+    U(i) = m * g * len * (1 - cos(X(1, i)));
+    Total(i) = T(i) + U(i);
+    
+    subplot(1,2,2);
+    
+    plot(T(1:i));
+    hold on
+    plot(U(1:i));
+    plot(Total(1:i));
+    
+    axis square;
+    title('energies');
+    xlabel('time step');
+    ylabel('energy');
+    legend('kinetic energy', 'potential energy', 'total energy', 'Location','northwest');
+    
+    hold off
+    
+    % update the state & energies
+    X(:,i+1) = odestep(X(:,i),@pend,h,'imp_euler',params);
+    
 end
